daad8cfb2c65efc39667f425a78390d8ef7dcb0d
Runan <125736765+Rx00000@users.noreply.github.com>
2023-07-04 14:19:31 +0200

    Add files via upload

---
+++ atm_info.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def return_valen_el(element):      
+    if element == "Ti":
+        return 4
+    if element == "Zr":
+        return 4   
+    if element == "Hf":
+        return 4
+    if element == "V":
+        return 5    
+    if element == "Nb":
+        return 5 
+    if element == "Ta":
+        return 5   
+    if element == "Cr":
+        return 6   
+    if element == "Mo":
+        return 6    
+    if element == "W":
+        return 6 
+    if element == "Sr":
+        return 2    
+    if element == "Ba":
+        return 2
+    if element == "Y":
+        return 3 
+    if element == "La":
+        return 3 
+    if element == "Mn":
+        return 7   
+    if element == "Fe":
+        return 8
+    if element == "Co":
+        return 9 
+    if element == "Ni":
+        return 10    
+    if element == "Cu":
+        return 11
+    if element == "Zn":
+        return 12   
+    if element == "Al":
+        return 3 
+    if element == "P":
+        return 5
+    if element == "Si":
+        return 4 
+    if element == "Sn":
+        return 4    
+    if element == "Sb":
+        return 5
+
+
+# In[ ]:
+
+
+def return_radius(element):      
+    if element == "Ti":
+        return 140
+    if element == "Zr":
+        return 155   
+    if element == "Hf":
+        return 155
+    if element == "V":
+        return 135    
+    if element == "Nb":
+        return 145 
+    if element == "Ta":
+        return 145   
+    if element == "Cr":
+        return 140   
+    if element == "Mo":
+        return 145    
+    if element == "W":
+        return 135 
+    if element == "Sr":
+        return 200    
+    if element == "Ba":
+        return 215
+    if element == "Y":
+        return 180 
+    if element == "La":
+        return 195 
+    if element == "Mn":
+        return 140   
+    if element == "Fe":
+        return 140
+    if element == "Co":
+        return 135 
+    if element == "Ni":
+        return 135    
+    if element == "Cu":
+        return 135
+    if element == "Zn":
+        return 135   
+    if element == "Al":
+        return 125
+    if element == "P":
+        return 100
+    if element == "Si":
+        return 110 
+    if element == "Sn":
+        return 145    
+    if element == "Sb":
+        return 145
+
+
+# In[ ]:
+
+
+def return_elec_neg(element):      
+    if element == "Ti":
+        return 0.123363699999999
+    if element == "Zr":
+        return 0.124888699999999   
+    if element == "Hf":
+        return 0.229987
+    if element == "V":
+        return 0.1273344    
+    if element == "Nb":
+        return 0.1280782 
+    if element == "Ta":
+        return 0.234580999999999   
+    if element == "Cr":
+        return 0.131305299999999   
+    if element == "Mo":
+        return 0.1312672    
+    if element == "W":
+        return 0.2390504 
+    if element == "Sr":
+        return 0.1185082    
+    if element == "Ba":
+        return 0.1586786
+    if element == "Y":
+        return 0.1216986 
+    if element == "La":
+        return 0.163142 
+    if element == "Mn":
+        return 0.1352844   
+    if element == "Fe":
+        return 0.1392532
+    if element == "Co":
+        return 0.143236299999999 
+    if element == "Ni":
+        return 0.1472068    
+    if element == "Cu":
+        return 0.1511716
+    if element == "Zn":
+        return 0.155151799999999   
+    if element == "Al":
+        return 0.150078 
+    if element == "P":
+        return 0.206931
+    if element == "Si":
+        return 0.178503 
+    if element == "Sn":
+        return 0.178052    
+    if element == "Sb":
+        return 0.191877399999999
+
